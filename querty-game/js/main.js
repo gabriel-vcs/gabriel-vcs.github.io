a1be768e8a3741315b702516577561dc66c757d0
Gabriel Gomez <ggomez.box@gmail.com>
2022-06-26 20:54:10 +0200

    generate random words all over the corners of the panel game

--- querty-game/js/main.js
+++ querty-game/js/main.js
@@ -2,26 +2,29 @@ class Game {
   constructor() {
     this.player = new Player();
     this.words = [];
-    this.level = 1; // 1..4
+    this.level = 4; // 1..4
     this.speed = 1; // 1..4
     this.intervalId = 0;
   }
 
   start() {
     this.createListeners();
+
     let timer = 0;
-    this.words.push(new Word());
+    this.words.push(new Word(this.level));
     this.intervalId = setInterval(() => {
       timer++;
-      if ((timer * 50) % 1000 === 0) {
+      if ((timer * 50) % 3000 === 0) {
+        this.words.push(new Word(this.level));
+        this.words.shift().elm.remove();
       }
-      // this.words.forEach((word) => {
-      // });
+      this.words.forEach((word) => {
+        word.moveElement();
+      });
     }, 1000 / 50);
   }
   createListeners() {
     window.addEventListener("keydown", (event) => {
-      console.log(event.key);
       if (event.key === "Escape") {
         clearInterval(this.intervalId);
       }
@@ -29,29 +32,8 @@ class Game {
   }
 }
 
-class DomElement {
-  constructor(tagName, position, rotation) {
-    this.rotation += 5;
-    if (tagName) {
-      this.elm = this.getNewElement(tagName);
-    } else {
-      this.elm = null;
-    }
-  }
-  getNewElement(tagName) {
-    console.log("this.position>>>", this.position);
-    // const elm = document.createElement(tagName);
-    // elm.position = this.getRndPosition();
-  }
-  getRndPosition() {}
-  rotate() {
-    this.elm.style.transform = "rotate(" + this.rotation + "deg)";
-  }
-}
-
-class Player extends DomElement {
+class Player {
   constructor() {
-    super(null, [0, 0], 45);
     this.health = 100;
     this.points = 0;
     this.elm = document.getElementById("player");
@@ -59,15 +41,91 @@ class Player extends DomElement {
   }
 }
 
-class Word extends DomElement {
-  constructor() {
-    super("span");
-    this.word = this.getRndWord();
-    this.distanceToPlayer = 100;
-    this.orientation = 0;
+class WordController {
+  constructor(level) {
+    this.level = level;
+    this.gridMaxXPos = 12;
+    this.gridMaxYPos = 12;
+    this.marginLeft = 5;
+    this.positionMap = this.getPositionMap();
   }
   getRndWord() {
-    return words[Math.floor(Math.random() * words.length)];
+    const words = [];
+    for (let i = 0; i < 4; i++) {
+      const rndWordIndex = Math.floor(Math.random() * data.length) + 1;
+      words.push(data[rndWordIndex]);
+    }
+    const asc = words.sort((a, b) => a.length - b.length);
+    return asc[this.level - 1];
+  }
+  getRndPosition() {
+    const rndIndex = Math.floor(Math.random() * this.positionMap.length);
+    return this.positionMap[rndIndex];
+  }
+  getPositionMap() {
+    const gameRect = document.getElementById("game").getBoundingClientRect();
+    const panelRect = document.getElementById("panel").getBoundingClientRect();
+    const width = gameRect.width;
+    const heigth = gameRect.height - panelRect.height;
+    const positionMap = [];
+    for (let i = 0; i < this.gridMaxXPos - 1; i++) {
+      positionMap.push(
+        [
+          Math.floor((width / this.gridMaxXPos) * i) + this.marginLeft,
+          Math.floor(heigth / this.gridMaxYPos),
+        ],
+        [
+          Math.floor((width / this.gridMaxXPos) * i) + this.marginLeft,
+          Math.floor(heigth - heigth / this.gridMaxYPos),
+        ]
+      );
+    }
+    for (let i = 2; i < this.gridMaxYPos - 1; i++) {
+      positionMap.push(
+        [this.marginLeft, Math.floor(heigth / this.gridMaxYPos) * i],
+        [
+          Math.floor(width - (width / this.gridMaxXPos) * 2) + this.marginLeft,
+          Math.floor(heigth / this.gridMaxYPos) * i,
+        ]
+      );
+    }
+    return positionMap;
+  }
+  rotate() {
+    this.elm.style.transform = "rotate(" + this.rotation + "deg)";
+  }
+}
+
+class Word extends WordController {
+  constructor(level) {
+    super(level);
+    this.elm = this.createNewElm();
+    this.distanceToPlayer = 100;
+    this.orientation = 0;
+    this.movesToPlayer = 10;
+  }
+  createNewElm() {
+    const wordElm = document.createElement("span");
+    wordElm.className = "word";
+    wordElm.innerText = this.getRndWord();
+    const rndPosition = this.getRndPosition();
+    wordElm.style.left = rndPosition[0] + "px";
+    wordElm.style.top = rndPosition[1] + "px";
+    const gameElm = document.getElementById("game");
+    gameElm.appendChild(wordElm);
+    return wordElm;
+  }
+  moveElement() {
+    const playerElem = document.getElementById("player");
+    console.log("this.playerElem.style.left>>>", playerElem.style.left);
+    // if (Math.abs(this.playerElem.style.left - this.elm.style.left) > 20) {
+    //   this.elm.style.left +=
+    //     (playerElem.style.left - this.elm.style.left) / this.movesToPlayer;
+    // }
+    // if (Math.abs(this.playerElem.style.top - this.elm.style.top) > 20) {
+    //   this.elm.style.top +=
+    //     (playerElem.style.top - this.elm.style.top) / this.movesToPlayer;
+    // }
   }
 }
 
